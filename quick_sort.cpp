a9a86e923b87763d4f374a010d2a2c98041ef71e
hsoloman <hsoloman@gmail.com>
2014-05-08 08:21:43 -0400

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-partition(vector<int>, int pivotIndex)
-{
+int partition (vector<int> & A, int lo, int hi){
+    int i(lo), j(hi + 1);
+    int pivot = A[lo];
+
+    while (1){
+        while ( A[++i] < pivot );
+        while ( A[--j] > pivot );
+        if (i >= j) break;
+        swap(A[i], A[j]);
+    }
+    swap( A[lo], A[j] );
+    return j;
+}
+
+void quickSort(vector<int>& A, int lo, int hi){
+    if (lo >= hi) return;
+    
+    int pivot = partition(A, lo, hi);
+    quickSort(A, lo, pivot -1);
+    quickSort(A, pivot+1, hi);
     
 }
 
-
 int main()
 {
-    vector<int> testSequence = {1, 2, 3, -1, 4, -10, 1, 20};
+    vector<int> test = {9,5,1,2,7,88,-4,2,15};
+    int pivot = partition(test, 0, test.size()-1);    
+    for (int i : test) cout << i << ", ";
     
+    cout << endl;
+    
+    vector<int> testqs = {9,5,1,2,7,88,-4,2,15};
+    quickSort(testqs, 0, testqs.size() -1);
+    for (int i : testqs) cout << i << ", ";
 
-    
-    return 0;
+   return 0;
 }
